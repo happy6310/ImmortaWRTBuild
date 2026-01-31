213dd788c2cd705edd7174852eea5ac339b83d0f
happy6310 <happy6310@gmail.com>
2026-01-31 21:18:14 +0800

    Update feeds-extra.sh

--- scripts/feeds-extra.sh
+++ scripts/feeds-extra.sh
@@ -3,9 +3,5 @@ set -e
 
 echo "[INFO] Apply extra feeds config..."
 
-sed -i 's/git.openwrt.org\/feed/github.com\/openwrt/g' feeds.conf.default
-sed -i 's/git.openwrt.org\/project/github.com\/openwrt/g' feeds.conf.default
-
-if ! grep -q "2305ipk" feeds.conf.default; then
-  echo "src-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk" >> feeds.conf.default
-fi
+## 添加自己的插件库
+sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
