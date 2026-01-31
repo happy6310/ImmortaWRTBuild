2d532c89bee5625c58af6c009ca0297291d435cf
happy6310 <happy6310@gmail.com>
2026-01-31 22:08:34 +0800

    Update feeds_extra.sh

--- scripts/feeds_extra.sh
+++ scripts/feeds_extra.sh
@@ -4,6 +4,5 @@ set -e
 echo "[INFO] Apply extra feeds config..."
 
 ## 添加自己的插件库
-# sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
+sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
 
-sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n"  ./wrt/scripts/feeds.conf.default
